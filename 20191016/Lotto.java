b3edca78da5c4accac781c38c0dfb287912d76b9
tjdud4123 <37131313+tjdud4123@users.noreply.github.com>
2019-11-13 20:23:12 +0900

    Update Lotto.java

--- 20191016/Lotto.java
+++ 20191016/Lotto.java
@@ -1,21 +1,24 @@
 package bean.user;
 
-public class Lotto {
-	public static void main(String[] args) {
-		int[] lotto = new int[6];
-		for(int i=0; i<6; i++) {
-			lotto[i] = (int)((Math.random()*45)+1);
-			for(int j=0; j<i; j++) {
-				if(lotto[j] == lotto[i]) {
+public class Lotto1 {
+	private String N;
+	public void setNum(String N) {
+		this.N = N;
+	}
+	public String getNum() {
+		int[] arr = new int[6];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=(int)(Math.random()*45)+1;
+			for(int j=0;j<i;j++) {
+				if(arr[i]==arr[j]) {
 					i--;
 					break;
 				}
 			}
 		}
-		
-		for(int value : lotto) {
-			System.out.println(value + " ");
+		for(int i=0;i<arr.length;i++){
+			N+=arr[i]+" ";
 		}
+		return N;
 	}
-
 }
