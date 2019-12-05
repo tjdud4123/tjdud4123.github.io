37f9279094e9a25f701e96883c22bcfe9bc7fc5c
tjdud4123 <37131313+tjdud4123@users.noreply.github.com>
2019-12-05 11:46:35 +0900

    Create S20171360DTO.java

---
+++ 20191204/S20171360DTO.java
@@ -0,0 +1,54 @@
+package sofa20171360pkg;
+
+public class S20171360DTO
+{
+	String sofa_brand;
+	String sofa_color;
+	String sofa_price;
+	String sofa_width;
+	String sofa_size;
+	String sofa_material;
+	String sofa_regdate;
+	public String getSofa_brand() {
+		return sofa_brand;
+	}
+	public void setSofa_brand(String sofa_brand) {
+		this.sofa_brand = sofa_brand;
+	}
+	public String getSofa_color() {
+		return sofa_color;
+	}
+	public void setSofa_color(String sofa_color) {
+		this.sofa_color = sofa_color;
+	}
+	public String getSofa_price() {
+		return sofa_price;
+	}
+	public void setSofa_price(String sofa_price) {
+		this.sofa_price = sofa_price;
+	}
+	public String getSofa_width() {
+		return sofa_width;
+	}
+	public void setSofa_width(String sofa_width) {
+		this.sofa_width = sofa_width;
+	}
+	public String getSofa_size() {
+		return sofa_size;
+	}
+	public void setSofa_size(String sofa_size) {
+		this.sofa_size = sofa_size;
+	}
+	public String getSofa_material() {
+		return sofa_material;
+	}
+	public void setSofa_material(String sofa_material) {
+		this.sofa_material = sofa_material;
+	}
+	public String getSofa_regdate() {
+		return sofa_regdate;
+	}
+	public void setSofa_regdate(String sofa_regdate) {
+		this.sofa_regdate = sofa_regdate;
+	}
+}
